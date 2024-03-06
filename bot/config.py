28e2518255b8631acf4a58d181239ea40a6ab005
udazzZZ <pirogova.dv96@mail.ru>
2024-03-06 11:49:18 +0300

    fix start

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,12 @@
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic import SecretStr
+#
+#
+# class Settings(BaseSettings):
+#     bot_token: SecretStr
+#
+#     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+#
+#
+# config = Settings()
 BOT_TOKEN = '6778324912:AAFt2hWsZzZdBFvqeztoY8HCjD8fqTjkTac'
--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,42 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher
-from aiogram.enums.parse_mode import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command, CommandStart
+from aiogram import F
+from aiogram.utils.formatting import (
+    Bold, as_list, as_marked_section
+)
+from dotenv import load_dotenv
+
+from bot.keyboards import kb, all_keyboards
 
 import config
-from handlers import router
 
+load_dotenv()
+
+bot = Bot(token=config.BOT_TOKEN, parse_mode="HTML")
+
+router = Router()
+dp = Dispatcher()
+dp.include_routers(router, all_keyboards.router1, all_keyboards.router2)
+
+@router.message(F.text, CommandStart())
+async def cmd_start(message: types.Message):
+    await message.answer(
+        "📈Привет! Я - <b>бот</b>, созданный на основе технологий "
+        "машинного обучения. Я умею прогнозировать "
+        "динамику цен акций. (Внимание! Ни я, ни мои создатели не можем "
+        "гарантировать достоверность и корректность прогноза)📈"
+    )
+
+    await message.answer(
+        "Выбери интересующий пункт:",
+        reply_markup=kb.select_data
+    )
 
 async def main():
-    bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
-    dp = Dispatcher(storage=MemoryStorage())
-    dp.include_router(router)
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
